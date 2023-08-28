5e2b9184e251cd54093082ada9612630a56f66df
J8ARIOS <137454106+J8ARIOS@users.noreply.github.com>
2023-08-27 23:13:01 -0500

    Add files via upload

---
+++ Proyectos/Hola.py
@@ -0,0 +1,18 @@
+print("Hola github")
+def calculate_area(radius):
+    pi = 3.14
+    return pi * radius**2
+
+radius = int(input("Enter the radius of the circle: "))
+area = calculate_area(radius)
+print("The area of the circle is: ", area)
+
+#LECTURA EN VOZ ALTA
+#PRINT
+#ERRORES
+#RUBBER DUCKY
+#BREAKPOINTS
+#DOCUMENTACION
+#EXPERIENCIA Y CREENCIAS
+
+#FUNCIONES
